7bb65ee7725730a108719f67e0e16482382fd12d
Rahul Bhanushali <imrahul@amazon.com>
2020-05-22 03:50:16 +0530

    Updated site title

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Rahul Bhanushali'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Personal blog | Rahul Bhanushali'
 
 export default function Layout({ children, home }) {
   return (
